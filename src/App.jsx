f626e302a9379b5ce892b66df03e78f710b3461f
Aleksandarjo <aleksandar.joovanoovic@gmail.com>
2024-04-15 16:41:14 +0200

    router combined with layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
+
 import Movies from "./pages/Movies";
 import Series from "./pages/Series";
+import Home from "./pages/Home";
 import Wishlist from "./pages/Wishlist";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./components/AppLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/movies",
-    element: <Movies />,
-  },
-  {
-    path: "/series",
-    element: <Series />,
-  },
-  {
-    path: "/wishlist",
-    element: <Wishlist />,
-  },
-  {
-    path: "*",
-    element: <PageNotFound />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/movies",
+        element: <Movies />,
+      },
+      {
+        path: "/series",
+        element: <Series />,
+      },
+      {
+        path: "/wishlist",
+        element: <Wishlist />,
+      },
+      {
+        path: "*",
+        element: <PageNotFound />,
+      },
+    ],
   },
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider router={router}>
+      <AppLayout></AppLayout>
+    </RouterProvider>
+  );
 };
 
 export default App;
--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,12 +1,19 @@
-import Sidebar from "./components/Sidebar";
+import { Outlet } from "react-router-dom";
+
+import Search from "./Search";
+import Sidebar from "./Sidebar";
 
 const AppLayout = () => {
   return (
     <div className="grid min-h-dvh grid-cols-[6rem_1fr]">
       <Sidebar />
       <div className="grid grid-rows-[2rem_1fr]">
-        <div className="bg-blue-600">header</div>
-        <div className="bg-red">main</div>
+        <div className="bg-blue-600">
+          <Search />
+        </div>
+        <div className="bg-red">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
