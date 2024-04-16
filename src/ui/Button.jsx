36128362d6f986dc71ac2f22747e6444728ce494
Aleksandarjo <aleksandar.joovanoovic@gmail.com>
2024-04-16 15:12:43 +0200

    created reusable button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ children, className, onClick }) => {
   return (
     <button
-      className={`body-m w-full rounded-xl border-none bg-red px-10 py-4 text-white focus:outline-none ${className || ""}`}
+      className={`body-m w-full rounded-xl border-none bg-red px-10 py-4 text-white transition-colors hover:bg-white hover:text-black focus:outline-none ${className || ""}`}
     >
       {children}
     </button>
